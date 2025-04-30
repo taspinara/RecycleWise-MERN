d8c24672e7f1fa7f05ff636c99657f0a0c6635eb
Ali <taspinara17@gmail.com>
2025-04-30 02:42:31 +0200

    Fix Jest config: remove extensionsToTreatAsEsm

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,7 +1,6 @@
 // server/jest.config.cjs
 module.exports = {
     testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.js'],    // .js dosyalarını ESM olarak işle
     transform: {},                       // hiçbir dönüşüm (Babel vs.) uygulama
 };
 // Jest, ESM modüllerini destekler, ancak bazı ayarlar gerekebilir.
