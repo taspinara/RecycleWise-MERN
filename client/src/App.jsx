aada04c21809a799a7c4af99a1c5b7d3b40003d1
Ali <taspinara17@gmail.com>
2025-04-30 05:28:28 +0200

    fix: added navbar to the App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,18 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Scan from './pages/Scan';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/scan" element={<Scan />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/scan" element={<Scan />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
